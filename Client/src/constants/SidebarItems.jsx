5c8bc55efe96363d64a4540dd13114c8d3e54d50
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-09-21 13:25:09 +0530

    constants are updated

---
+++ Client/src/constants/SidebarItems.jsx
@@ -0,0 +1,40 @@
+export const SidebarMainItems = [
+    // {
+    //   title: "Dashboard",
+    //   path: "/private/dashboard",
+    //   icon: "MdOutlineSpaceDashboard",
+    // },
+    {
+      title: "Derma Detection",
+      path: "/private/derma-detection",
+      icon: "PiStethoscopeFill",
+    },
+    {
+      title: "Skin Care",
+      path: "/private/skin-care",
+      icon: "PiMagicWand",
+    },
+    {
+      title: "Search Disease",
+      path: "/private/search-disease",
+      icon: "BsBriefcase",
+    },
+    {
+      title: "Symptom Checker",
+      path: "/private/symptom-checker",
+      icon: "BiCheckShield",
+    },
+    {
+      title: "Health Analytics",
+      path: "/private/health-analytics",
+      icon: "BiLineChart",
+    },
+    {
+      title: "Community Forum",
+      path: "/private/community-forum",
+      icon: "BsChatSquareHeart",
+    },
+  ];
+  
+  
+  
