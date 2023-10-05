8c18749a9fa30ef0e515dc1465f09286b71bb3ef
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-10-05 21:23:34 +0530

    small fix

---
+++ Client/src/constants/LandingSectionTitle.jsx
@@ -0,0 +1,22 @@
+export const LandingSectionTitle = [
+    {
+        title: "About Us",
+        description: "Enjoy our best AI features to help you with Derma Disease Detection"
+    },
+    {
+        title: "Features",
+        description: "Enjoy our best AI features to help you with Derma Disease Detection"
+    },
+    {
+        title: "Why Choose Us",
+        description: "Enjoy our best AI features to help you with Derma Disease Detection"
+    },
+    {
+        title: "FAQ",
+        description: "Enjoy our best AI features to help you with Derma Disease Detection"
+    },
+    {
+        title: "Contact Us",
+        description: "Enjoy our best AI features to help you with Derma Disease Detection"
+    },
+]
