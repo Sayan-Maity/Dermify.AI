0a35b0a089aa413c950859b139b5f3f665e584e4
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-11-27 14:09:25 +0530

    changed to palm llm

--- Server/controllers/EducationPrompt.js
+++ Server/controllers/EducationPrompt.js
@@ -1,50 +1,66 @@
-const {Configuration, OpenAIApi } = require("openai");
+const { DiscussServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleAuth } = require("google-auth-library");
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_KEY,
+const PALM_API_KEY = process.env.PALM_KEY;
+const MODEL_NAME = "models/chat-bison-001";
+
+const client = new DiscussServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(PALM_API_KEY),
 });
-
-const openai = new OpenAIApi(configuration);
 
 module.exports.educationPrompt = async (req, res) => {
   const { disease } = req.body;
   try {
-    const gptResponse = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: `I am sending you a skin disease name, upon that, send me detail of that disease in 5 different points:
-          1.  Name (name of the disease)
-          2. Description (a brief 200 words description about the disease)
-          3. Symptoms (give atleast 10 symptoms of the disease)
-          4. Causes (give atlease 5 causes of the disease)
-          5. Prevention (give atleast 3 best possible way for the prevention of this disease)
-          
-          Name of Disease = ${disease}
-          
-          give me the output in json :
-          [
-            {
+    const result = await client.generateMessage({
+      model: MODEL_NAME,
+      temperature: 0.25,
+      top_k: 40,
+      top_p: 0.95,
+      candidateCount: 1,
+      prompt: {
+        context: "",
+        examples: [],
+        messages: [
+          {
+            content: `I am sending you a skin disease name, upon that return me a json having the following fields:
+            1.  Name (name of the disease)
+            2. Description (a brief 200 words description about the disease)
+            3. Symptoms (give atleast 10 symptoms of the disease)
+            4. Causes (give atlease 5 causes of the disease)
+            5. Prevention (give atleast 3 best possible way for the prevention of this disease)
+            
+            Disease name = ${disease}
+            
+            give me the output in json :{
               "name" : "",
               "description": "",
               "symptoms": ["", "", "", "", "", "", "", "", "", ""],
               "causes": ["", "", "", "", ""],
               "prevention": ["", "", ""]
-            },
-          ]`,
-        },
-      ],
-      max_tokens: 2000,
-      temperature: 0.9,
+            }`,
+          },
+        ],
+      },
     });
 
-    if (gptResponse) {
-      // console.log(gptResponse.choices[0].message);
-      res.status(200).json({ gptPrompt: gptResponse.data.choices[0].message });
+    if (result && Array.isArray(result) && result.length > 0) {
+      const firstCandidate = result[0].candidates[0];
+
+      if (firstCandidate) {
+        const content = firstCandidate.content;
+        // console.log(content);
+        return res.json({ content });
+        // jsonParser(content);
+      } else {
+        console.log("No valid candidate found");
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    } else {
+      console.log("Invalid response format");
+      res.status(500).json({ error: "Internal Server Error" });
     }
   } catch (err) {
-    // console.log(err);
-    res.status(500).json({ error: "Server Error" });
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- Server/controllers/RecommendationPrompt.js
+++ Server/controllers/RecommendationPrompt.js
@@ -1,33 +1,39 @@
-const {Configuration, OpenAIApi } = require("openai");
+const { DiscussServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleAuth } = require("google-auth-library");
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_KEY,
+const PALM_API_KEY = process.env.PALM_KEY;
+const MODEL_NAME = "models/chat-bison-001";
+
+const client = new DiscussServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(PALM_API_KEY),
 });
-
-const openai = new OpenAIApi(configuration);
 
 module.exports.recommendationPrompt = async (req, res) => {
   const { skinType, skinConcern } = req.body;
   try {
-    const gptResponse = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: `I am sending you my skin type and skin concerns and upon that you have to recommend skin care routine ,
-          1. Description (a brief and elaborative info about the skin type and skin concern user is facing)
-          2. Specialized Treatment (give atleast 5 ways for the treatment)
-          3. Daily Skin Care Routine (give atleast 5 skin care daily routine for the below skin type and skin concern)
-          4. Weekly Skin Care Routine (give atleast 5 skin care weekly routine for the below skin type and skin concern)
-          5. Hydration and Diet (give atleast 5 tips to remain hydrated and follow a proper diet)
-          6. Advice (give some advices)
-          
-          Skin Type = ${skinType}
-          Skin Concerns = ${skinConcern}
-          
-          give me the output in json :
-          [
-            {
+    const result = await client.generateMessage({
+      model: MODEL_NAME,
+      temperature: 0.25,
+      top_k: 40,
+      top_p: 0.95,
+      candidateCount: 1,
+      prompt: {
+        context: "",
+        examples: [],
+        messages: [
+          {
+            content: `I am sending you my skin type and skin concerns, recommend me a skin care routine and return me a json having the following fields :
+            1. Description (a brief and elaborative info about the skin type and skin concern user is facing)
+            2. Specialized Treatment (give atleast 5 ways for the treatment)
+            3. Daily Skin Care Routine (give atleast 5 skin care daily routine for the below skin type and skin concern)
+            4. Weekly Skin Care Routine (give atleast 5 skin care weekly routine for the below skin type and skin concern)
+            5. Hydration and Diet (give atleast 5 tips to remain hydrated and follow a proper diet)
+            6. Advice (give some advices)
+            
+            Skin Type = ${skinType}
+            Skin Concerns = ${skinConcern}
+            
+            give me the output in this json structure :{{
               "skinType": "",
               "skinConcerns": "",
               "description": "",
@@ -36,20 +42,30 @@ module.exports.recommendationPrompt = async (req, res) => {
               "weeklyRoutine": ["", "", "", "", ""],
               "hydrationAndDiet": ["", "", ""],
               "advice": ["", "", ""]
-            },
-          ]`,
-        },
-      ],
-      max_tokens: 2000,
-      temperature: 0.9,
+            }}`,
+          },
+        ],
+      },
     });
 
-    if (gptResponse) {
-      //   console.log(gptResponse.choices[0].message);
-      res.status(200).json({ gptPrompt: gptResponse.data.choices[0].message });
+    if (result && Array.isArray(result) && result.length > 0) {
+      const firstCandidate = result[0].candidates[0];
+
+      if (firstCandidate) {
+        const content = firstCandidate.content;
+        // console.log(content);
+        return res.json({ content });
+        // jsonParser(content);
+      } else {
+        console.log("No valid candidate found");
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    } else {
+      console.log("Invalid response format");
+      res.status(500).json({ error: "Internal Server Error" });
     }
   } catch (err) {
-    // console.log(err);
-    res.status(500).json({ error: "Server Error" });
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
