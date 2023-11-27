8908ecafa2f0d40cf8f804294daa7d47f19e0284
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-11-27 13:04:15 +0530

    changed the port number to : 5001

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv");
 dotenv.config();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 const cors = require("cors")
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
