5fd2f10135ecaf64ca3f436c67f57ceb57c1687f
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-11-27 14:05:48 +0530

    removed symptom-checker route

--- Client/src/routes/CustomRoutes.jsx
+++ Client/src/routes/CustomRoutes.jsx
@@ -5,13 +5,12 @@ import Login from "../pages/authPages/Login";
 import Register from "../pages/authPages/Register";
 import Dashboard from "../pages/dashboardPages/Dashboard";
 import DermaDetection from "../pages/dashboardPages/DermaDetection";
-import Sidebar from "../components/Sidebar";
+import Private from "../components/Private";
 import PrivateRoutes from "./PrivateRoutes";
 import ContactUS from "../pages/LandingPages/ContactUs";
 import Developers from "../pages/LandingPages/Developers";
 import SearchDisease from "../pages/dashboardPages/SearchDisease";
 import SkinCare from "../pages/dashboardPages/SkinCare";
-import SymptomChecker from "../pages/dashboardPages/SymptomChecker";
 import HealthAnalytics from "../pages/dashboardPages/HealthAnalytics";
 import CommunityForum from "../pages/dashboardPages/CommunityForum";
 
@@ -28,12 +27,11 @@ const CustomRoutes = () => {
       <Route exact path="*" element={<WrongPage/>} />
 
       {/* -----------------------  PRIVATE ROUTES  --------------------- */}
-      <Route exact path="/private" element={<PrivateRoutes element={Sidebar} />}>
+      <Route exact path="/private" element={<PrivateRoutes element={Private} />}>
         <Route exact path="/private/search-disease" element={<PrivateRoutes element={SearchDisease} />} />
         <Route exact path="/private/skin-care" element={<PrivateRoutes element={SkinCare} />} />
         <Route exact path="/private/dashboard" element={<PrivateRoutes element={Dashboard} />} />
         <Route exact path="/private/derma-detection" element={<PrivateRoutes element={DermaDetection} />} />
-        <Route exact path="/private/symptom-checker" element={<PrivateRoutes element={SymptomChecker} />} />
         <Route exact path="/private/health-analytics" element={<PrivateRoutes element={HealthAnalytics} />} />
         <Route exact path="/private/community-forum" element={<PrivateRoutes element={CommunityForum} />} />
       </Route>
