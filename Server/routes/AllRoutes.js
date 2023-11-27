45dee3d90dcad504033c0c18ab9db4007f2d589c
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-11-27 14:11:48 +0530

    added new routes for community forum

--- Server/routes/AllRoutes.js
+++ Server/routes/AllRoutes.js
@@ -2,11 +2,14 @@ const express = require("express")
 const { dermaPrompt } = require("../controllers/DermaPrompt")
 const { educationPrompt } = require("../controllers/EducationPrompt")
 const { recommendationPrompt } = require("../controllers/RecommendationPrompt")
+const { getCommunityForum, postCommunityForum } = require("../controllers/CommunityForum")
 const router = express.Router()
 
 router.post("/dermaFinalPrompt", dermaPrompt)
 router.post("/educationPrompt", educationPrompt)
 router.post("/recommendationPrompt", recommendationPrompt)
+router.post("/postCommunityForum", postCommunityForum)
+router.get("/getCommunityForum", getCommunityForum)
 
 
 module.exports = router
