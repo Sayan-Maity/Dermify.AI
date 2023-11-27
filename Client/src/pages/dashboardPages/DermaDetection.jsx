71611204c8432e8ba42ecf38473e1ec9731e294c
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-11-27 14:04:38 +0530

    changed openai wrapper to palm wrapper

--- Client/src/pages/dashboardPages/DermaDetection.jsx
+++ Client/src/pages/dashboardPages/DermaDetection.jsx
@@ -23,7 +23,7 @@ const DermaDetection = () => {
   const [dataUri, setDataUri] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const [conditionsData, setConditionsData] = useState([]);
+  const [conditionsData, setConditionsData] = useState({});
   const [language, setLanguage] = useState("English");
   const [symptomPrompt, setSymptomPrompt] = useState("");
 
@@ -92,18 +92,41 @@ const DermaDetection = () => {
       );
       if (response.status === 200) {
         setLoading(false);
-        console.log(response.data.gptPrompt.content);
-        const jsonResponse = response.data.gptPrompt.content;
-
-        // Parse the JSON string into a JavaScript object
-        const parsedData = JSON.parse(jsonResponse);
-        setConditionsData(parsedData);
+        console.log(response);
+        setConditionsData(jsonParser(response.data));
       }
     } catch (error) {
       setLoading(false);
       console.log(error);
     }
   };
+
+  const jsonParser = (response) => {
+    try {
+      if (response && response.content) {
+        const jsonRegex = /```json\s*([\s\S]+)\s*```/; // Define a regular expression
+        const match = response.content.match(jsonRegex); // Extract the JSON string
+
+        if (match && match[1]) {
+          const trimmedJsonStr = match[1].trim(); // Remove leading and trailing whitespaces
+          const jsonObj = JSON.parse(trimmedJsonStr);
+
+          // console.log("here =>", jsonObj);
+          return jsonObj;
+        } else {
+          console.error('No JSON object found in the response.');
+          return null;
+        }
+      } else {
+        console.error('Invalid response format. No content property found.');
+        return null;
+      }
+    } catch (error) {
+      console.error('Error parsing JSON:', error.message);
+      return null;
+    }
+  };
+
   return (
     <Flex
       p="2rem"
@@ -142,30 +165,32 @@ const DermaDetection = () => {
               id="imageFile"
             />
           </label>
-          <Flex
-            flexDir="row"
-            gap="1rem"
-            borderRadius="5px"
-            border="1px solid #e4e6ea"
-            p="0.5rem"
-            overflowX={browsedImagesArray.length > 3 ? "scroll" : "hidden"}
-          >
-            {browsedImagesArray.map((image, index) => (
-              <Image
-                border={selectedImage === image ? "3px solid #3ce2ad" : "none"}
-                cursor="pointer"
-                onClick={() => handleImageChange(index)}
-                objectFit="cover"
-                borderRadius="5px"
-                height="5rem"
-                width="5rem"
-                key={index}
-                src={URL.createObjectURL(image)}
-                alt={`Selected Image ${index}`}
-                className="uploaded-image"
-              />
-            ))}
-          </Flex>
+          {browsedImagesArray.length > 0 && (
+            <Flex
+              flexDir="row"
+              gap="1rem"
+              borderRadius="5px"
+              border="1px solid #e4e6ea"
+              p="0.5rem"
+              overflowX={browsedImagesArray.length > 3 ? "scroll" : "hidden"}
+            >
+              {browsedImagesArray.map((image, index) => (
+                <Image
+                  border={selectedImage === image ? "3px solid #3ce2ad" : "none"}
+                  cursor="pointer"
+                  onClick={() => handleImageChange(index)}
+                  objectFit="cover"
+                  borderRadius="5px"
+                  height="5rem"
+                  width="5rem"
+                  key={index}
+                  src={URL.createObjectURL(image)}
+                  alt={`Selected Image ${index}`}
+                  className="uploaded-image"
+                />
+              ))}
+            </Flex>
+          )}
 
           <Select
             value={language}
@@ -209,7 +234,7 @@ const DermaDetection = () => {
                 {loading ? (
                   "Detecting..."
                 ) :
-                  conditionsData[0]?.name
+                  conditionsData?.name
                 }
               </Text>
             </Flex>
@@ -225,7 +250,7 @@ const DermaDetection = () => {
                 {loading ? (
                   "Detecting..."
                 ) :
-                  conditionsData[0]?.description
+                  conditionsData?.description
                 }
               </Text>
             </Flex>
@@ -238,7 +263,7 @@ const DermaDetection = () => {
             >
               <Text fontWeight="500">Communicable</Text>
               <Text border="1px solid #e4e6ea" p="1rem" borderRadius="5px">
-                {loading ? "Detecting..." : conditionsData[0]?.communicable}
+                {loading ? "Detecting..." : conditionsData?.communicable}
               </Text>
 
             </Flex>
@@ -252,7 +277,7 @@ const DermaDetection = () => {
               <Text fontWeight="500">Symptoms</Text>
               <Flex border="1px solid #e4e6ea" p="1rem" borderRadius="5px">
                 {loading ? "Detecting..." : (<UnorderedList>
-                  {conditionsData[0]?.symptoms?.map((symptom, index) => (
+                  {conditionsData?.symptoms?.map((symptom, index) => (
                     <ListItem key={index}>{symptom}</ListItem>
                   ))}
                 </UnorderedList>)}
@@ -268,7 +293,7 @@ const DermaDetection = () => {
               <Text fontWeight="500">Causes</Text>
               <Flex border="1px solid #e4e6ea" p="1rem" borderRadius="5px">
                 {loading ? "Detecting..." : (<UnorderedList>
-                  {conditionsData[0]?.causes?.map((cause, index) => (
+                  {conditionsData?.causes?.map((cause, index) => (
                     <ListItem key={index}>{cause}</ListItem>
                   ))}
                 </UnorderedList>)}
@@ -284,7 +309,7 @@ const DermaDetection = () => {
               <Text fontWeight="500">Treatment</Text>
               <Flex border="1px solid #e4e6ea" p="1rem" borderRadius="5px">
                 {loading ? "Detecting..." : (<UnorderedList>
-                  {conditionsData[0]?.treatment?.map((treatment, index) => (
+                  {conditionsData?.treatment?.map((treatment, index) => (
                     <ListItem key={index}>{treatment}</ListItem>
                   ))}
                 </UnorderedList>)}
@@ -299,8 +324,8 @@ const DermaDetection = () => {
             >
               <Text fontWeight="500">External Link</Text>
               <Flex border="1px solid #e4e6ea" p="1rem" borderRadius="5px">
-                <Link to={conditionsData[0]?.link} target="_blank" border="1px solid #e4e6ea" p="1rem" borderRadius="5px">
-                  {loading ? "Detecting..." : conditionsData[0]?.link}
+                <Link to={conditionsData?.link} target="_blank" border="1px solid #e4e6ea" p="1rem" borderRadius="5px">
+                  {loading ? "Detecting..." : conditionsData?.link}
                 </Link>
               </Flex>
             </Flex>
