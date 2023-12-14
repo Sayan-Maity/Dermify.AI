f6155df09596368d8c3a2253ba0cffe836edd14b
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-12-14 14:01:21 +0530

    increased the token lifespan

---
+++ Client/src/components/Private.jsx
@@ -0,0 +1,27 @@
+import {
+  Flex,
+  useTheme,
+} from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+
+const Private = () => {
+  const theme = useTheme();
+  return (
+    <Flex
+      h="100vh"
+      flexDir="row"
+      justifyContent="center"
+      alignItems="flex-start"
+      borderRight={`1px solid ${theme.colors.border}`}
+    >
+      <Sidebar />
+      <Flex w="100%">
+        <Outlet />
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Private;
