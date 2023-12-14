f7c0357df0c76cd8abcdd8f49140de027613a574
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2023-12-14 14:43:33 +0530

    added more route endpoints

--- Server/routes/UserRoutes.js
+++ Server/routes/UserRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const { diseaseHistory } = require("../controllers/AuthController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const { getLifestyleAnalyticsData, postLifestyleAnalyticsData, diseaseHistory, getSkinCareRoutineData, getStressData, postStressData, getDietData, postDietData, getWaterIntakeData, postWaterIntakeData, postSkinCareRoutineData } = require("../controllers/UserController");
 const router = express.Router();
 
 router.post("/saveDisease", authMiddleware, diseaseHistory);
+router.get("/getLifestyleAnalyticsData", authMiddleware, getLifestyleAnalyticsData);
+router.post("/postLifestyleAnalyticsData", authMiddleware, postLifestyleAnalyticsData);
+router.get("/getSkinCareRoutineData", authMiddleware, getSkinCareRoutineData);
+router.post("/postSkinCareRoutineData", authMiddleware, postSkinCareRoutineData);
+router.get("/getStressData", authMiddleware, getStressData);
+router.post("/postStressData", authMiddleware, postStressData);
+router.get("/getDietData", authMiddleware, getDietData);
+router.post("/postDietData", authMiddleware, postDietData);
+router.get("/getWaterIntakeData", authMiddleware, getWaterIntakeData);
+router.post("/postWaterIntakeData", authMiddleware, postWaterIntakeData);
 
 module.exports = router;
 
